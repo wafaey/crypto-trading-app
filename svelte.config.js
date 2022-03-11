7e600514e9d4b5579c65395ee48de0bff62a1f18
Omar Wafaey <omarwafaey54@gmail.com>
2022-03-11 14:02:26 +0200

    Fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
-//import adapter from '@sveltejs/adapter-node';
+//import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,8 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ out: 'my-output-directory' })
+   // adapter: adapter(),
   },
 };
 
